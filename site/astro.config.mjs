06a269905bcac95f358e14e8e6a997ad64d1c512
Employee 001 <employee+001@ehq.cl>
2025-11-26 23:19:20 -0300

    Changes

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
+// site/astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://misterfunable.github.io/blog',
+  base: '/blog',
   vite: {
     plugins: [tailwindcss()]
   }
